7e15784cf2f9118f9d6429c3ee3e0bbb4e06076a
JoseLucioS <dragaolucio@gmail.com>
2021-07-20 20:52:57 -0300

    Add: classe Display, README. Sistema funcionando.

---
+++ src/HUD/Display.java
@@ -0,0 +1,16 @@
+package HUD;
+
+import jdk.swing.interop.SwingInterOpUtils;
+
+public class Display {
+    public void mostrarOpcoes(){
+        System.out.println("-------BANCO DO PROGRESSO-------");
+        System.out.println("PORQUE OS TEMPOS MUDAM MUITO...");
+        System.out.println("Digite 1 - para cadastrar uma conta");
+        System.out.println("Digite 2 - para consultar um saldo");
+        System.out.println("Digite 3 - para creditar");
+        System.out.println("Digite 4 - para debitar");
+        System.out.println("Digite 5 - para transferência");
+        System.out.println("Digite 0 - para encerrar operações");
+    }
+}
--- src/Main/Main.java
+++ src/Main/Main.java
@@ -2,28 +2,92 @@ package Main;
 
 import Banco.Banco;
 import Contas.Conta;
+import HUD.Display;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Banco banco = new Banco();
         Conta c = new Conta("123456-1");
+        String numeroDaConta, numeroContaOrigem, numeroContaDestino;
+        double valor;
 
-        banco.cadastrarConta(c.getNumero());
-        banco.cadastrarConta("123456-2");
-        banco.creditar(c.getNumero(), 345.57);
-        banco.debitar("123456-2", 200.10);
+        Scanner leitor = new Scanner(System.in);
 
-        System.out.println("antes da transferencia:");
-        System.out.println("saldo: " + banco.consultarSaldo(c.getNumero()));
-        System.out.println("saldo: " + banco.consultarSaldo("123456-2"));
+        int opcao = -1;
 
-        banco.transferir(c.getNumero(), "123456-2", 300);
+        Display display = new Display();
 
-        System.out.println("depois da transferencia:");
-        System.out.println("saldo: " + banco.consultarSaldo(c.getNumero()));
-        System.out.println("saldo: " + banco.consultarSaldo("123456-2"));
+        //TODO colocar um "if" para verificar o êxito das funções booleanas (creditar, debitar, transferir)
+        while(opcao != 0){
+            display.mostrarOpcoes();
+            opcao = leitor.nextInt();
 
-        System.out.println(banco.contas.size());
+            switch (opcao){
+                case 1:
+
+                    System.out.println("Por favor, informe um numero para a conta:");
+                    numeroDaConta = leitor.next();
+
+                    banco.cadastrarConta(numeroDaConta);
+
+                    break;
+
+                case 2:
+
+                    System.out.println("Por favor, informe o numero da conta:");
+                    numeroDaConta = leitor.next();
+
+                    double resultado = banco.consultarSaldo(numeroDaConta);
+
+                    System.out.println("Seu saldo é: " + resultado);
+
+                    break;
+
+                case 3:
+
+                    System.out.println("Por favor, informe o numero da conta:");
+                    numeroDaConta = leitor.next();
+                    System.out.println("Informe o valor para creditar:");
+                    valor = leitor.nextDouble();
+
+                    banco.creditar(numeroDaConta, valor);
+
+                    break;
+
+                case 4:
+
+                    System.out.println("Por favor, informe o numero da conta:");
+                    numeroDaConta = leitor.next();
+                    System.out.println("Informe o valor para debitar:");
+                    valor = leitor.nextDouble();
+
+                    banco.debitar(numeroDaConta, valor);
+
+                    break;
+
+                case 5:
+
+                    System.out.println("Por favor, informe o numero da conta de origem:");
+                    numeroContaOrigem = leitor.next();
+                    System.out.println("Por favor, informe o numero da conta de destino:");
+                    numeroContaDestino = leitor.next();
+                    System.out.println("valor a ser transferido:");
+                    valor = leitor.nextDouble();
+
+                    banco.transferir(numeroContaOrigem, numeroContaDestino, valor);
+
+                    break;
+                case 0:
+
+                    System.out.println("Encerrando o sistema...");
+
+                    break;
+
+                default:
+                    System.out.println("Operação inválida!");
+            }
+        }
 
     }
 }
